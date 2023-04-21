74886e5a13dcd327ac95d4db4cbe1b20585933f3
NDNewell <nicholas.newell@pm.me>
2023-04-21 09:01:03 -0600

    feat: add method to check if straight

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -31,6 +31,31 @@ class PokerHand {
     return cardValues;
   }
 
+  isFlush() {
+    const suit = this.cards[0][1];
+
+    for (const card of this.cards) {
+      if (card[1] !== suit) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  isStraight() {
+  const cardValues = this.cards.map(card => this.convertValueToNumber(card[0]));
+  cardValues.sort((a, b) => a - b);
+
+  for (let i = 1; i < cardValues.length; i++) {
+    if (cardValues[i] !== cardValues[i - 1] + 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
   getRank() {
     // Implement poker hand ranking
     return 'Royal Flush';
