6007d97e1f5629019d1107ac142f23a0cd00eb98
NDNewell <nicholas.newell@pm.me>
2023-04-21 08:44:05 -0600

    feat: assign values to card types

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,5 +1,24 @@
 class PokerHand {
-  constructor() {}
+  constructor(hand) {
+    this.cards = hand.split(' ');
+  }
+
+  convertValueToNumber(value) {
+    switch (value) {
+      case 'A':
+        return 14;
+      case 'K':
+        return 13;
+      case 'Q':
+        return 12;
+      case 'J':
+        return 11;
+      case 'T':
+        return 10;
+      default:
+        return parseInt(value, 10);
+    }
+  }
 
   getRank() {
     // Implement poker hand ranking
